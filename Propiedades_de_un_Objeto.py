216c07fa05802023817c46caf6f5aaf6936278f7
josefernandoperezgarcia1998 <josefernandoperezgarcia98@gmail.com>
2022-02-18 17:24:17 -0600

    Definiciones de <Propiedades de un objeto>

---
+++ Propiedades_de_un_Objeto.py
@@ -0,0 +1,12 @@
+"""
+Propiedades de un objeto
+1.- Las propiedades de cada obj son distintas entre ellos, a pesar de que tengan el mismo nombre de la propiedad cada
+    una es distinta porque pertenecen a objetos diferentes
+
+    Ejem:   el objeto 1 Curso() tiene de propiedad nombre: "PHP" y de categoria "Programación web"
+            el objeto 2 Curso() tiene de propiedad nombre "Swift" y de categoria "Programación móvil, iOS"
+
+2.- Al valor de todas las propiedades de los objetos se le llama "Estado del objeto"
+    Usualmente un objeto proveerá de métodos para modificar sus propiedades al ser usado.
+    El estado del objeto puede almacenar enteros, cadenas, arreglos, etc..
+"""
