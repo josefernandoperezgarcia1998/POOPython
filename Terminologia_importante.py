3690bd89436dd0cc106d76ee7384577986bd0aa1
josefernandoperezgarcia1998 <josefernandoperezgarcia98@gmail.com>
2022-02-18 17:11:29 -0600

    Se definen los conceptos de <Instanciar, Instancia, Estado y Estado de un objeto

---
+++ Terminologia_importante.py
@@ -0,0 +1,10 @@
+"""
+Se dice que Instanciar es: Cuando se instancia un objeto de una clase cuando nos referimos a crear un objeto de una clase.
+
+Instancia: Objeto creado a partir de una clase.
+
+Estado: Conjunto de valores almacenados que pueden ser modificados mientras el programa se ejecuta.
+Estado de un objeto: Valores que en ese momento tiene las propiedades del objeto.
+                     Se pueden modificar sus propiedades, ya que cada objeto tiene sus propiedades independientes.
+
+"""
