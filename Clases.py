c8852fff5349a7c502d38e6dc33e050928aa5951
josefernandoperezgarcia1998 <josefernandoperezgarcia98@gmail.com>
2022-02-18 16:58:51 -0600

    Definición general de POO, Objeto y Clases

---
+++ Clases.py
@@ -0,0 +1,24 @@
+"""
+La forma más popular de crear objetos es a partir de clases.
+
+Clase: Son documentos donde se define el comportamiento del objeto, las propiedades que tiene, acciones que se pueden
+        realizar y como se puede utilizar el objeto.
+
+Ejemplo de una clase:
+
+    Class Usuario
+        correoElectronico - string
+        nombre - string
+        nombreDelCanal - string
+        edad - int
+        publicar()
+        subirFoto()
+        añadirAmigo()
+    finClase..
+
+Apartir de una clase se crean los objetos y cada objeto posee un estado propio y distinto que se producen por una clase
+
+Ejemplo: Dos marcadores/plumones... Pueden ser de diferentes tamaños, pueden tener diferentes colores, pueden ser de agua ó no
+
+
+"""
---
+++ main.py
@@ -0,0 +1,7 @@
+
+#Paradigma:  Conjunto de cosas que sirven como un modelo o ejemplo
+
+#POO: Paradigma de programación es un paradigma basado en creación de objetos que pueden contener datos y funciones.
+    # Temas: Abstracción, Encapsulamiento, Herencia y Polimorfismo.
+    # Un objeto: Combinación de propiedades y atributos, junto con una serie de métodos que puede ejecutar.
+    # Alan Kay: Acuñó por primera vez el término de POO en los 60's.
