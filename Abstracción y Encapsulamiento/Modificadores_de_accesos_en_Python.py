5ab33b26e19507941d4306d2af3006e169477ebf
josefernandoperezgarcia1998 <josefernandoperezgarcia98@gmail.com>
2022-02-18 20:43:14 -0600

    Modificadores de acceso en python, public, protected y private

---
+++ Abstracción y Encapsulamiento/Modificadores_de_accesos_en_Python.py
@@ -0,0 +1,14 @@
+"""
+En Python se sabe que cuando una propiedad es creada pues es pública
+
+    propiedad publica "numero"
+
+En caso de que esa variable se tenga que hacer "protegida" se debe de agregar un guión bajo antes de la misma
+
+    propiedad protegida "_numero"
+
+De esta forma protegida no se debe de acceder desde una instancia a una propiedad de un objeto
+
+Por último, para que una variable se tenga que hacer "privada" se debe agregar doble guión bajo "__numero"
+Además de que, se debe de agregar el nombre de la clase en donde se creó esa propiedad "_Empleado__numero"
+"""
