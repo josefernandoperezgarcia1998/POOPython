b270518bd35feaf627f24175e8ad99c4b6cc3441
josefernandoperezgarcia1998 <josefernandoperezgarcia98@gmail.com>
2022-02-18 18:11:45 -0600

    Accediento a los atributos de un objeto de la clase Usuario y modificando el nombre

--- Definir_una_clase_en_python.py
+++ Definir_una_clase_en_python.py
@@ -1,6 +1,16 @@
-#Se crea una clase llamada curso
-class Curso:
-    pass
+# Se crea una clase llamada curso
+class Usuario:
+    nombre = "Fernando"
 
-#Se instancia un objeto de la clase curso
-curso_python = Curso()
+#Se crea un objeto de tipo usuario y se le cambia el nombre
+fernando = Usuario()
+fernando.nombre = "José"
+
+
+#Se crea un objeto de tipo usuario y se le asigna un nombre
+liliana = Usuario()
+liliana.nombre = "Sofia"
+
+#De ambos objetos se imprimen los valores de la propiedad nombre
+print(fernando.nombre)
+print(liliana.nombre)
