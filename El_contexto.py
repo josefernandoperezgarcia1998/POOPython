3437fb7c27df996f48d120a6f2c30832a8dd9767
josefernandoperezgarcia1998 <josefernandoperezgarcia98@gmail.com>
2022-02-18 18:42:47 -0600

    Definición del <contexto ó valor> de un objeto

---
+++ El_contexto.py
@@ -0,0 +1,10 @@
+"""
+El contexto es el valor de las propiedades de un objeto
+En python el contexto se usa la palabra reservada => self
+En otros lenguajes de programación se utiliza la palabra reservada => this
+
+Por ejemplo:
+
+se tiene un objeto Carro() que una propiedad es "color", y su valor (o sea el contexto) es "rojo"
+Se tiene otro objeto Lápiz() que su propiedad sea "tipo" y su valor (o sea el contexto) es "dibujar"
+"""
