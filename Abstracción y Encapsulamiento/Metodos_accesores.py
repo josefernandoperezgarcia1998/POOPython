8c752ea6ed8a609719e61028d3f30fb6c755a6be
josefernandoperezgarcia1998 <josefernandoperezgarcia98@gmail.com>
2022-02-19 13:18:18 -0600

    Métodos accesores <get|set>

---
+++ Abstracción y Encapsulamiento/Metodos_accesores.py
@@ -0,0 +1,14 @@
+"""
+Tema de encapsulamiento...
+
+Para los métodos que se crean en el código para modificar los valores de las propiedades de los objetos se llama "Metodos accesores"
+
+Existen 2 tipos: Get y Set.
+
+Normalmente un get y un set se define para una misma propiedad.
+
+El método get se encarga se retornar el valor actual de la propiedad.
+El método set se encarga de asignar un nuevo valor a esa propiedad.
+
+
+"""
