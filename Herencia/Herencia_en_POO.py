475d92d52fc958d274b1bf870fd87b756d08a921
josefernandoperezgarcia1998 <josefernandoperezgarcia98@gmail.com>
2022-02-18 19:07:26 -0600

    Definición Herencia

---
+++ Herencia/Herencia_en_POO.py
@@ -0,0 +1,26 @@
+"""
+
+Herencia: Permite que un objeto herede las propiedades y métodos de un objeto que se llame PAdre ó Base.
+A partir de ese objeto PAdre se puede agregar, quitar ó modificar métodos y propiedades en el objeto que se identifica como hijo
+
+Herencia permite identificar una jerarquía.
+
+Animal - Padre
+    Puede respirar
+    Puede reproducirse
+    Tiene edad
+    Fecha de nacimiento
+
+Mamifero - Hijo
+    Puede respirar
+    Puede reproducirse
+    Tiene una edad
+    Fecha de nacimiento
+    Puede amamantar
+    Tienen un periodo de gestación
+
+Perro - Especifico
+    Todas las propiedades de ANIMAL
+    Todas las propiedades de MAMIFERO
+
+"""
