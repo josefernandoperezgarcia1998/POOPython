3d98b7aa0d7412539526f31bff04b1e68b306584
josefernandoperezgarcia1998 <josefernandoperezgarcia98@gmail.com>
2022-02-18 20:26:59 -0600

    Encapsulamiento definiciones y caracteristicas

---
+++ Abstracción y Encapsulamiento/Encapsulamiento.py
@@ -0,0 +1,14 @@
+"""
+Encapsulamiento:
+
+Los datos y funciones que manipulan los datos se envian como una sola unidad.
+
+En vez de exponer el estado del objeto, se exponen métodos que alteran el estado.
+
+De un objeto que disponga ciertos métodos va a poder hacerse la manipulación a datos.
+
+No se debe exponer la propiedad, se debe de exponer el método.
+
+
+
+"""
