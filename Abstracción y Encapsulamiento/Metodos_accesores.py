61d95debe6127aa4ab8ac2770ebf5aeadab4c59a
josefernandoperezgarcia1998 <josefernandoperezgarcia98@gmail.com>
2022-02-19 13:54:51 -0600

    Aplicando método get y set para acceder a la propiedad de un objeto privado

--- Abstracción y Encapsulamiento/Metodos_accesores.py
+++ Abstracción y Encapsulamiento/Metodos_accesores.py
@@ -12,3 +12,45 @@ El método set se encarga de asignar un nuevo valor a esa propiedad.
 
 
 """
+
+class Usuario:  # Clase Padre
+    __edad = 0
+    def __init__(self, nombre):
+        self.nombre = nombre
+
+    def saludar(self, saludo):
+        print(saludo + self.nombre)
+
+    #Método getter
+    @property
+    def edad(self):
+         return self.__edad
+
+    #Método setter
+    @edad.setter
+    def edad(self,valor_edad):
+        if(valor_edad < 0):
+            raise ValueError('Edad no puede ser menor que 0')
+        self.__edad = valor_edad
+
+class Empleado(Usuario):  # Clase hijo
+    salario = 0
+
+    #Método Setter
+    def modificar_salario(self, salario):
+        self.salario = salario
+
+    #Método getter
+    def ver_salario(self):
+        print(self.salario)
+
+    def saludar(self, saludo):
+        super().saludar("Saludo con super para ")
+        print("Mi nombre es: "+self.nombre+" y gano:" +str(self.salario))
+
+empleado = Empleado("José Fernando")
+empleado.edad = 23 #La edad se manda a llamar con el método setter, ya no con la propiedad original de la clase Padre Usuario
+#empleado.edad = -2 #La edad no puede ser < 0
+print(empleado.edad) #La edad se manda a llamar como propiedad, ya no cómo el método
+
+
