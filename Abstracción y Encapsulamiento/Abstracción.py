d0aeb040741379c288b4a44819c214c248d4c988
josefernandoperezgarcia1998 <josefernandoperezgarcia98@gmail.com>
2022-02-19 14:21:42 -0600

    Definición de abstracción

---
+++ Abstracción y Encapsulamiento/Abstracción.py
@@ -0,0 +1,13 @@
+"""
+Abstracción es tomar un tema complejo, ignorar los detalles y ver los detalles desde un parnoama general
+
+Ejemplo:
+Problema ó circunstancia: Tengo hambre
+Solución: Ve a comer
+
+Se toma de manera general ó muy abstracta la forma de resolver el problema, ya ue no se abunda en detalles.
+
+1.-En POO la abstracción es:
+        Significa ocultar detalles innecesarios y exponer lo escenecial, de manera que se enfoque que es lo que hará el objeto
+        en lugar de como lo va a realizar.
+"""
